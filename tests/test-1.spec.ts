1ac0a5e1e5140f68ccd31b4dcbc4afb23e3d936d
Mateo <mateo.botero.upegui@ey.com>
2026-03-30 15:58:10 -0500

    Delete all test cases

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from './baseTest';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-});
-
-//COMENTARIO DE PRUEBA PARA VER SI FUNCIONA EL COMENTARIO EN EL CODIGO
